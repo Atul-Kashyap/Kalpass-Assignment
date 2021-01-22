87d0c297a7ba2403f96efe040cbb5873636d6cc6
Atul Kashyap <atulkashyap1208@gmail.com>
2021-01-22 20:41:44 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,39 @@
+ var myVideo = document.getElementById("video1"); 
+
+function playPause() { 
+  if (myVideo.paused) 
+    myVideo.play(); 
+  else 
+    myVideo.pause(); 
+} 
+
+function makeBig() { 
+    myVideo.width = 1420; 
+} 
+
+function makeSmall() { 
+    myVideo.width = 320; 
+} 
+
+function makeNormal() { 
+    myVideo.width = 420; 
+} 
+
+
+function myFunction() {
+  myVideo = document.getElementById("myDropdown").classList.toggle("show");;
+  
+}
+// window.onclick = function(event) {
+//   if (!event.target.matches('.dropbtn')) {
+//     var dropdowns = document.getElementsByClassName("dropdown-content");
+//     var i;
+//     for (i = 0; i < dropdowns.length; i++) {
+//       var openDropdown = dropdowns[i];
+//       if (openDropdown.classList.contains('show')) {
+//         openDropdown.classList.remove('show');
+//       }
+//     }
+//   }
+// }
+
